c2c148bed8785fc809fb8a65db131542901c95fc
unknown <J71379@yachiyo-eng.co.jp>
2026-04-02 17:36:10 +0900

    update init

--- src/geometry/spatial/point/__init__.py
+++ src/geometry/spatial/point/__init__.py
@@ -1,9 +1,7 @@
 from .point import Point3D
 from .points import Points3D
-from .sequential import SequentialPoint3D
 
 __all__ = [
     "Point3D", 
-    "Points3D", 
-    "SequentialPoint3D"
+    "Points3D"
     ]
